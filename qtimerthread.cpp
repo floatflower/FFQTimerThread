b436266e6ee914995c92480a4479f576440675bc
floatflower <zxc110888asd@gmail.com>
2017-05-08 11:17:01 +0800

    Shapshot v0.0

--- qtimerthread.cpp
+++ qtimerthread.cpp
@@ -1,6 +1,29 @@
 #include "qtimerthread.h"
 
-
-FF::QTimerThread::QTimerThread()
+FF::QTimerThread::QTimerThread(QObject *parent)
+    :QThread(parent)
 {
+    this->m_timer = new QTimer;
+    this->m_timerThread = new QTimerThread;
+    m_timer->moveToThread(m_timerThread);
+    connect(m_timer, SIGNAL(timeout()), this, SIGNAL(timeout()));
+    connect(this, SIGNAL(timerStart()), m_timer, SLOT(start()));
+}
+
+FF::QTimerThread::~QTimerThread()
+{
+    delete this->m_timer;
+    delete this->m_timerThread;
+}
+
+void FF::QTimerThread::setInterval(int msec)
+{
+    m_timer->setInterval(msec);
+}
+
+void FF::QTimerThread::run()
+{
+    m_timerThread->start();
+    emit timerStart();
+    exec();
 }
--- qtimerthread.h
+++ qtimerthread.h
@@ -1,14 +1,29 @@
 #ifndef QTIMERTHREAD_H
 #define QTIMERTHREAD_H
 
+#include <QObject>
+#include <QThread>
+#include <QTimer>
+
 #include "ffqtimerthread_global.h"
 
 namespace FF {
-class FFQTIMERTHREADSHARED_EXPORT QTimerThread
+class FFQTIMERTHREADSHARED_EXPORT QTimerThread : public QThread
 {
-
+    Q_OBJECT
 public:
-    QTimerThread();
+    QTimerThread(QObject *parent = 0);
+    ~QTimerThread();
+    void setInterval(int msec);
+    virtual void run();
+signals:
+    void timeout();
+    void timerStart();
+public slots:
+    virtual void doRoutine() = 0;
+private:
+    QTimer *m_timer;
+    QThread *m_timerThread;
 };
 }
 
